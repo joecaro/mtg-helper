c84ebcb37363fde5ab8864231fefba436f4bc7ab
Joseph Carothers <jsc11598@gmail.com>
2024-09-19 02:15:31 -0400

    fix lint errors

--- src/app/(client).tsx
+++ src/app/(client).tsx
@@ -11,7 +11,7 @@ const queryClient = new QueryClient({
     },
 });
 
-export default function ClientApp({ children }: PropsWithChildren<{}>) {
+export default function ClientApp({ children }: PropsWithChildren) {
     return (
         <QueryClientProvider client={queryClient}>
             {children}
