3864149dc6a4ac8a9fde544ab8b3e425f94a3bac
Joseph Carothers <jsc11598@gmail.com>
2024-09-29 17:03:01 -0400

    viewport meta

--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -1,5 +1,17 @@
 import GameUI from '@/components/game-tracker'
 import { GameStateProvider } from '@/components/game-tracker/player-state'
+import { Metadata, Viewport } from 'next'
+
+export const metadata: Metadata = {
+	title: 'MTG Help Me',
+	description: 'Elevating the Magic: The Gathering Game Stats Tracking',
+}
+
+export const viewport: Viewport = {
+	width: 'device-width',
+	initialScale: 1,
+	viewportFit: 'cover',
+}
 
 export default function Page(): JSX.Element {
 	return (
